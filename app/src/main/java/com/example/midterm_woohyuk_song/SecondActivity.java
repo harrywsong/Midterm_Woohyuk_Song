63e22a53d458332ac46c18e4fbd6f8c434e97e47
Woohyuk Song <harrywsong@naver.com>
2025-10-30 10:52:54 -0400

    finished secondactivity

--- app/src/main/java/com/example/midterm_woohyuk_song/SecondActivity.java
+++ app/src/main/java/com/example/midterm_woohyuk_song/SecondActivity.java
@@ -1,4 +1,28 @@
 package com.example.midterm_woohyuk_song;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ListView historyListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        historyListView = findViewById(R.id.historyListView);
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MainActivity.historyList);
+        historyListView.setAdapter(adapter);
+    }
 }
